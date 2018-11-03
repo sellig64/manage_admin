84f41f49131c88525e0b9beddd98886928e0067a
sellig64 <sellig-004@hotmail.fr>
2018-11-03 16:31:57 +0100

    just for fun git log!

--- README.md
+++ README.md
@@ -3,11 +3,41 @@ Hi everybody, sorry for my English,
 
 --- This script ping servers via a file containing multiple IP addresses (one per line) and send a report email.
 
---- this script is created with the use of a gmail account in the outbox ('gmail'SMTP server).
-Make sure the "Allow less secure apps" tab is enabled in your Google Account (URL: https://myaccount.google.com/security).
+--- this script is created with the use of a gmail account in the outbox (SMTP server 'gmail' in this example).
+
+Configuration:
+
+1) Make sure the "Allow less secure apps" tab is enabled in your Google Account (URL: https://myaccount.google.com/security).
+
+2) create a file named "text_python.txt" or modify the code line 19 to change the name, insert the ip address of your servers (one per line) and indicate the address of the file on line 17.
+
+3) Indicate the address of the desired SMTP server and its port on line 63.
+
+4) Indicate the login and password of your email account on line 65.
+
+5) Indicate the sending email line 66.
+
+6) Indicate the receipt (s) line (s) in line 67 in quotation marks and separated by commas.
+
+7) Line 69 will be the title message of the mail, to modify if you want.
 
 ------------------------------------------------------------------------------------------------------
 --- ce script envoie une requête ping à des serveurs via un fichier contenant plusieurs adresses IP (une par ligne) et envoie un e-mail de rapport.
 
---- ce script est créé avec l'utilisation d'un compte gmail dans la boîte d'envoi (serveur SMTP 'gmail').
-Assurez-vous que l'onglet "Autoriser les applications moins sécurisées" est activé dans votre compte Google (URL: https://myaccount.google.com/security).
+--- ce script est créé avec l'utilisation d'un compte gmail dans la boîte d'envoi (serveur SMTP 'gmail' dans cet exemple).
+
+Configuration:
+
+1) Assurez-vous que l'onglet "Autoriser les applications moins sécurisées" est activé dans votre compte Google (URL: https://myaccount.google.com/security).
+
+2) créer un fichier nommé "text_python.txt" ou modifier le code ligne 19 pour changer le nom, y déposer l'adresse ip de vos serveurs (une par ligne) et indiquer l'adresse du fichier à la ligne 17.
+
+3) Indiquer l'adresse du serveur smtp souhaité ainsi que son port à la ligne 63.
+
+4) Indiquer le login et le mot de passe de votre compte mail à la ligne 65.
+
+5) Indiquer l'email d'envoi ligne 66.
+
+6) Indiquer le ou les emails de réception(s) ligne 67 entre guillemets et séparé par des virgules.
+
+7) La ligne 69 sera le message d'intitulé du mail, à modifier si vous voulez.
--- script.py
+++ script.py
@@ -14,15 +14,15 @@ import trace
 print("Hello world!\nVous avez lancé le logiciel de test de connexion des serveur sur le réseau!")
 
 
-os.chdir("/home/sellig/")  #déplacement dans le dossier contenant la liste
-
-with open("test_python.txt","r") as ma_liste:    #récupération et test des éléments d'une liste pré-établie:
+os.chdir("/home/sellig/")  #move to the folder containing the list / délacement dans le dossier contenant la liste
+				
+with open("test_python.txt","r") as ma_liste:    #retrieval and testing of elements from a pre-established list: / récupération et test des éléments d'une liste pré-établie: 
     texte = ma_liste.read()
     print(texte)
     ma_liste1 = texte.split()
     rec2 = len(ma_liste1)
     print(rec2)
-with open("data.html","w") as code_html:                #création d' un fichier html
+with open("data.html","w") as code_html:                #creating an html file / création d' un fichier html
     code_html.write("""
         <html>
         <head>
@@ -33,7 +33,7 @@ with open("data.html","w") as code_html:                #création d' un fichier
 
 x = 0
 
-while x != rec2 :                                    #ping + traceroute des addresses de la liste  et écriture du résultat dans le fichier html
+while x != rec2 :               #ping + traceroute the list's addresses and write the result to the html file / ping + traceroute des addresses de la liste  et écriture 							     #du résultat dans le fichier html
     rep = os.system("ping -c 1 " + ma_liste1[x])
     rep1 = os.system("traceroute " + ma_liste1[x] +" > trace.txt")
     with open("trace.txt","r") as trace:
@@ -49,36 +49,36 @@ while x != rec2 :                                    #ping + traceroute des addr
     ma_liste2.close()
     x = x+1
     
-with open("data.html","a") as code_html_fin:            #code de fin du fichier html
+with open("data.html","a") as code_html_fin:            #end code of the html file / code de fin du fichier html
     code_html_fin.write(""" 
     </body>
     </html>
 
 """)
-with open("data.html","r") as data:                 #Envoi du fichier html / mail
+with open("data.html","r") as data:                 #Sending the html file by mail / Envoi du fichier html par mail
     final = data.read()
 
 
-server = smtplib.SMTP()
+server = smtplib.SMTP()                     #Connection to the server and information useful information / connection au serveur et renseignement des informations utiles.
 server.connect("smtp.gmail.com", 587)
 server.starttls()
-server.login("EMAIL SENDBOX" , "PASSWORD")
+server.login("LOGIN SENDBOX" , "PASSWORD")
 fromaddr= "EMAIL SENDBOX"
-toaddr="EMAIL RECEPTION"
+toaddr="EMAIL RECEPTION","AUTRE EMAIL DE RECEPTION"
 
 sujet = "RAPPORT QUOTIDIEN"
 
 html = final
 
-msg = MIMEMultipart("alternative")
+msg = MIMEMultipart("alternative")  #element information variables requested from an email in html format / variables de renseignements des éléments demandé d' un email sous 							#format html
 msg["Subject"] = sujet
 msg["From"] = fromaddr
 msg["To"] = ",".join(toaddr)
 part = MIMEText(html, "html")
 msg.attach(part)
-try:
+try:                                                            # conformity test / test de conformité.
     server.sendmail(fromaddr, toaddr, msg.as_string())
-except smtplib.SMTPException as e:                              #Récuprération des erreurs
+except smtplib.SMTPException as e:                              #Recuperation of errors / récuprération des erreurs
         print(e)
 
 server.quit()
