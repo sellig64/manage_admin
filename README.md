8a7bc5a38e2482b090d370d7c243e495cf9d603d
sellig64 <sellig-004@hotmail.fr>
2018-11-15 19:21:28 +0100

    just for fun git add .!

--- README.md
+++ README.md
@@ -1,43 +1,63 @@
-# task_auto_python_admin
+# task_auto_python_admin             
+
+# for all suggestions : bibi@gmail.com
 Hi everybody, sorry for my English,
 
---- This script ping servers via a file containing multiple IP addresses (one per line) and send a report email.
+--- This script sends a ping + a traceroute + a port scan to servers via a file containing multiple IP addresses (one per line) and sends a report email while creating a replacement server.
 
---- this script is created with the use of a gmail account in the outbox (SMTP server 'gmail' in this example).
+--- This script is created with the use of a gmail account in the outbox (SMTP server 'gmail' in this example).
+
+--- This script works on Debian type servers.
+
+--- This script requires the installation of vitualbox and the creation of virtual machine of the same OS as the server to be replaced with an opening of user account, automatic and the basic pre-installation of the services concerned by each server as well as the creation of a functional backup and authorizing connection to the root account via ssh connecting with pre-configured ssh keys.
 
 Configuration:
 
 1) Make sure the "Allow less secure apps" tab is enabled in your Google Account (URL: https://myaccount.google.com/security).
 
-2) create a file named "text_python.txt" or modify the code line 19 to change the name, insert the ip address of your servers (one per line) and indicate the address of the file on line 17.
+2) indicate when running the script the name of the backup virtual machine and its ip address line 14 and 16.
 
-3) Indicate the address of the desired SMTP server and its port on line 63.
+3) indicate the path where the server backup is in .tar.gz format line 19 and 20
 
-4) Indicate the login and password of your email account on line 65.
+4) create a file named "test_python.txt" or modify the code line 29 to change the name, insert the ip address of your servers (one per line) and indicate the address of the file on line 27.
 
-5) Indicate the sending email line 66.
+5) Indicate the address of the desired SMTP server and its port on line 81.
 
-6) Indicate the receipt (s) line (s) in line 67 in quotation marks and separated by commas.
+6) Indicate the login and password of your email account on line 83.
 
-7) Line 69 will be the title message of the mail, to modify if you want.
+7) Indicate the sending email line 84.
+
+8) Indicate the receipt (s) line (s) in line 85 in quotation marks and separated by commas.
+
+9) Line 87 will be the title message of the mail, to modify if you want.
 
 ------------------------------------------------------------------------------------------------------
---- ce script envoie une requête ping à des serveurs via un fichier contenant plusieurs adresses IP (une par ligne) et envoie un e-mail de rapport.
+Bonjour à tous,                 
+# pour toutes les suggestions : bibi@gmail.com
+--- ce script envoie une requête ping + un traceroute + un nmap à des serveurs via un fichier contenant plusieurs adresses IP (une par ligne) et envoie un e-mail de rapport tout en créant un serveur de remplacement.
 
 --- ce script est créé avec l'utilisation d'un compte gmail dans la boîte d'envoi (serveur SMTP 'gmail' dans cet exemple).
+
+--- ce script fonctionne sur les serveurs type debian.
+
+--- ce script nécessite l'installation de virtualbox et la créaton de machine virtuelle du même OS que le serveur à remplacer avec une ouverture de compte utilisateur automatique et la pré-installation de base des services concernés par chaque serveur ainsi que la création d'une backup fonctionnelle et de l'autorisation de connection au compte root via ssh se connectant avec des clés ssh pré-configurées.
 
 Configuration:
 
 1) Assurez-vous que l'onglet "Autoriser les applications moins sécurisées" est activé dans votre compte Google (URL: https://myaccount.google.com/security).
 
-2) créer un fichier nommé "text_python.txt" ou modifier le code ligne 19 pour changer le nom, y déposer l'adresse ip de vos serveurs (une par ligne) et indiquer l'adresse du fichier à la ligne 17.
+2) indiquer lors de l'execution du script le nom de la machine virtuelle de secours et son adresse ip ligne 14 et 16.
 
-3) Indiquer l'adresse du serveur smtp souhaité ainsi que son port à la ligne 63.
+3) indiquer le chemin où se situe la sauvegarde du serveur au format .tar.gz ligne 19 et 20
 
-4) Indiquer le login et le mot de passe de votre compte mail à la ligne 65.
+4) Créer un fichier nommé "test_python.txt" ou modifier le code ligne 29 pour changer le nom, y déposer l'adresse ip de vos serveurs (une par ligne) et indiquer l'adresse du fichier à la ligne 27.
 
-5) Indiquer l'email d'envoi ligne 66.
+5) Indiquer l'adresse du serveur smtp souhaité ainsi que son port à la ligne 81.
 
-6) Indiquer le ou les emails de réception(s) ligne 67 entre guillemets et séparé par des virgules.
+6) Indiquer le login et le mot de passe de votre compte mail à la ligne 83.
 
-7) La ligne 69 sera le message d'intitulé du mail, à modifier si vous voulez.
+7) Indiquer l'email d'envoi ligne 84.
+
+8) Indiquer le ou les emails de réception(s) ligne 85 entre guillemets et séparé par des virgules.
+
+9) La ligne 87 sera le message d'intitulé du mail, à modifier si vous voulez.
--- script.py
+++ script.py
@@ -9,20 +9,30 @@ from email.mime.text import MIMEText
 import trace
 
 
-
-
-print("Hello world!\nVous avez lancé le logiciel de test de connexion des serveur sur le réseau!")
+def check_serveur():                        #Launch function of a backup virtual server / fonction de lancement d'un serveur virtuel de secour
+    print("ERROR : Un serveur ne répond pas, entrez le nom de votre serveur fantôme à lancer : ")
+    fantome = input()   
+    print("Entrez l'IP de se serveur : ")
+    virtual_ip = input()    
+    os.system("vboxmanage startvm "+ fantome )
+    time.sleep(60)
+    os.system("cd /home/sellig/serveur_backup/ ")
+    os.system("scp -r /home/sellig/serveur_backup/"+ ma_liste1[x] + ".tar.gz root@" + virtual_ip + ":/ ")
+    os.system("ssh root@" + virtual_ip+ " ' sleep 30 && cd / && tar -xvpzf " + ma_liste1[x] + ".tar.gz -C / && reboot'")
+    
+    
+print("Hello world!\nVous avez lancé le logiciel de test de connexion des serveurs sur le réseau!")
 
 
 os.chdir("/home/sellig/")  #move to the folder containing the list / délacement dans le dossier contenant la liste
-				
-with open("test_python.txt","r") as ma_liste:    #retrieval and testing of elements from a pre-established list: / récupération et test des éléments d'une liste pré-établie: 
+
+with open("test_python.txt","r") as ma_liste:   #retrieval and testing of elements from a pre-established list: / récupération et test des éléments d'une liste pré-établie:
     texte = ma_liste.read()
     print(texte)
     ma_liste1 = texte.split()
     rec2 = len(ma_liste1)
-    print(rec2)
-with open("data.html","w") as code_html:                #creating an html file / création d' un fichier html
+    print( rec2 )
+with open("data.html","w") as code_html:        #creating an html file / création d' un fichier html
     code_html.write("""
         <html>
         <head>
@@ -33,21 +43,29 @@ with open("data.html","w") as code_html:                #creating an html file /
 
 x = 0
 
-while x != rec2 :               #ping + traceroute the list's addresses and write the result to the html file / ping + traceroute des addresses de la liste  et écriture 							     #du résultat dans le fichier html
+while x != rec2 :   #ping + traceroute + nmap the list's addresses and write the result to the html file / ping + traceroute + nmap des addresses de la liste  et écriture 
     rep = os.system("ping -c 1 " + ma_liste1[x])
     rep1 = os.system("traceroute " + ma_liste1[x] +" > trace.txt")
+    rep2 = os.system("nmap -Pn " + ma_liste1[x] + " > nmap.txt")
+    
+    with open("nmap.txt","r") as nmap:
+        scanPort = nmap.read()
+    
     with open("trace.txt","r") as trace:
         traceroute = trace.read()
+    
     if rep == 0:
         ma_liste2 = open("data.html","a")
-        ma_liste2.write("\n" + "<p>&emsp;&emsp;serveur &emsp;&emsp;-------->&emsp;&emsp;" + ma_liste1[x] + "&emsp;&emsp;-------->&emsp;&emsp;" "<font color='green'> Connecté </font>&emsp;&emsp;<br>" + traceroute + "</br></p>")    
+        ma_liste2.write("\n" + "<p>&emsp;&emsp;serveur &emsp;&emsp;-------->&emsp;&emsp;" + ma_liste1[x] + "&emsp;&emsp;-------->&emsp;&emsp;" "<font color='green'> Connecté </font>&emsp;&emsp;<br>""<font color='purple'> traceroute :</font> " + traceroute + "</br><br>""<font color='blue'> nmap : </font>" + scanPort + "</br></p>")    
+    
     else:
         ma_liste2 = open("data.html","a")
-        ma_liste2.write("\n" + "<p>&emsp;&emsp;serveur &emsp;&emsp;-------->&emsp;&emsp;"+ ma_liste1[x] + "&emsp;&emsp;-------->&emsp;&emsp;" "<font color='red'> Déconnecté </font>&emsp;&emsp;<br>" + traceroute + "</br></p>")
-
-    time.sleep(3)
-    ma_liste2.close()
+        ma_liste2.write("\n" + "<p>&emsp;&emsp;serveur &emsp;&emsp;-------->&emsp;&emsp;"+ ma_liste1[x] + "&emsp;&emsp;-------->&emsp;&emsp;" "<font color='red'> Déconnecté </font>&emsp;&emsp;<br>""<font color='purple'> traceroute :</font>" + traceroute + "</br><br>""<font color='blue'> nmap : </font>" + scanPort + "</br></p>")   
+        check_serveur()
+        
+    time.sleep(3)        
     x = x+1
+    ma_liste2.close()
     
 with open("data.html","a") as code_html_fin:            #end code of the html file / code de fin du fichier html
     code_html_fin.write(""" 
@@ -59,24 +77,24 @@ with open("data.html","r") as data:                 #Sending the html file by ma
     final = data.read()
 
 
-server = smtplib.SMTP()                     #Connection to the server and information useful information / connection au serveur et renseignement des informations utiles.
+server = smtplib.SMTP()         #Connection to the server and information useful information / connection au serveur et renseignement des informations utiles.
 server.connect("smtp.gmail.com", 587)
 server.starttls()
-server.login("LOGIN SENDBOX" , "PASSWORD")
+server.login("EMAIL SENDBOX" , "PASSWORD")
 fromaddr= "EMAIL SENDBOX"
-toaddr="EMAIL RECEPTION","AUTRE EMAIL DE RECEPTION"
+toaddr="EMAIL OF RECEPTION"
 
 sujet = "RAPPORT QUOTIDIEN"
 
-html = final
+html = final        #element information variables requested from an email in html format / variables de renseignements des éléments demandé d' un email sous html
 
-msg = MIMEMultipart("alternative")  #element information variables requested from an email in html format / variables de renseignements des éléments demandé d' un email sous 							#format html
+msg = MIMEMultipart("alternative")
 msg["Subject"] = sujet
 msg["From"] = fromaddr
 msg["To"] = ",".join(toaddr)
 part = MIMEText(html, "html")
 msg.attach(part)
-try:                                                            # conformity test / test de conformité.
+try:                    # conformity test / test de conformité.
     server.sendmail(fromaddr, toaddr, msg.as_string())
 except smtplib.SMTPException as e:                              #Recuperation of errors / récuprération des erreurs
         print(e)
